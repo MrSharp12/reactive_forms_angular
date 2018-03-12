a186c55bda3e8d343d85fa8c8ac20b2d25deeaba
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-03-12 09:56:02 -0400

    array of form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
 
 
 
@@ -20,17 +21,27 @@ export class AppComponent implements OnInit {
   //remember reactive forms are created programatically
   //first argument is the default, the second is the validators
   //if you want to pass multiple validators, make an array
+  //controls are all added in ngOnInit
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }), 
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      //FormArray holds an array of controls
+      'hobbies': new FormArray([new FormControl])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    //we need to tell ts that this is of type FormArray, so we won't get any errors
+    //by placing it all in parens, we are telling angular that it is a form array
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 }
