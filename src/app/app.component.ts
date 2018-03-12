9b0625cb845e575155c6aa75a2979a3a4580bbcf
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-03-12 10:15:23 -0400

    creating custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   //we are required then to place the ngOnInit method
   //this intializes it before the template is rendered
@@ -25,7 +26,8 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        //we have to bind forbiddenNames to username so it refers to the class 'username'
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }), 
       'gender': new FormControl('male'),
@@ -43,5 +45,20 @@ export class AppComponent implements OnInit {
     //we need to tell ts that this is of type FormArray, so we won't get any errors
     //by placing it all in parens, we are telling angular that it is a form array
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+//for a vaildator to work, we have to enter control as the argument
+//it also needs to return something, a js object
+//it should have any key, and the value of the key/value pair should be a boolean
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    //a problem we are running into is we are checking if control value is part of the array
+    //this will return -1 if it is not part
+    //-1 is interperted as true
+    //so set up the logic to check if it is not equal to -1
+    //meaning we did find it, and it is invalid
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    //must return nothing is validation is successful
+    return null;
   }
 }
