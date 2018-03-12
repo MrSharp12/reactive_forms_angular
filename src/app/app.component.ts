e8e4fed4de0b6a531646c92c126a6dc8a7a81854
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-11 22:45:09 -0400

    creating a form in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+//first set implements OnInit lifecycle
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+
+  //we are required then to place the ngOnInit method
+  //this intializes it before the template is rendered
+  //we need to create a new FormGroup
+  //once we have passing in a js object, then we have our first form
+  //remember reactive forms are created programatically
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
 }
